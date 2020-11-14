36b2bdaddc63ee7761666b1dbe986daddc45325d
Karina <karinaa121213@gmail.com>
2020-11-14 22:12:19 +0200

    removed unused plugins and library in package.json, paths changed

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -1,8 +1,9 @@
 module.exports = function () {
 	$.gulp.task('html', function () {
 		return $.gulp.src('src/*.html')
-				.pipe($.gp.htmlmin({collapseWhitespace: true}))
-				.pipe($.gulp.dest('build/'))
-				.on('end', $.bs.reload);
+			.pipe($.gp.htmlmin({collapseWhitespace: true}))
+			.pipe($.gp.replace('../', ''))
+			.pipe($.gulp.dest('build/'))
+			.on('end', $.bs.reload);
 	});
 }
